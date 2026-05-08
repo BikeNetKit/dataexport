b6ef32785c3d10a7900a316e4618d6d9ff5dfb7b
Michael Szell <michael@szell.net>
2026-05-08 09:19:25 +0200

    Add slug and make parameters consistent

--- scripts/exportalldata_onecity.py
+++ scripts/exportalldata_onecity.py
@@ -3,10 +3,14 @@ Example for exporting all growbikenet data for one city.
 
 Parameters
 ----------
-nominatimstring : str, optional, default Barcelona
-    The string to search for in nominatim/OSM.
+city_name : str
+    Name of the city that the analysis should be performed on. This is the query string used to fetch the data from nominatim. Overruled (for data fetching) if city_boundary_file is set.
+export_data_slug : str, optional, default None
+    If not set to None, the city_name will be slugified and used as the slug in the filename of the data export
 export_file_format : str, optional, default "geojson"
     File format for the data export. Default "geojson", also possible "gpkg". If exporting as geojson, generates extra files for seed points and city boundary. If exporting as gkpg, these are added all in one file as extra layers.
+city_boundary_file : (str | None), default None
+    If not set to None, the study area will be selected from the (Multi)Polygon provided in the city_boundary_file shape file, ideally in unprojected latitude-longitude degrees (EPSG:4326), but EPSG:3857 also works. For example, "./tests/test_data/copenhagen.shp".
 
 Notes
 -------
@@ -16,31 +20,34 @@ Exports data into four files:
 [slug]-closeness_centrality-grid.gpkg
 [slug]-closeness_centrality-rail.gpkg
     Data is saved into the current working directory.
-    slug is a string id created out of nominatimstring.
+    slug is a string id created out of city_name.
 
 Examples
 --------
->>> python exportalldata_onecity.py Barcelona gpkg
+>>> python exportalldata_onecity.py Barcelona Barcelona gpkg
 """
 
 import growbikenet as gbn
 import sys
 from slugify import slugify
 
-nominatimstring = "Barcelona"
 city_name = "Barcelona"
+export_data_slug = "Barcelona"
 export_file_format = "geojson"
+city_boundary_file = None
 
 if len(sys.argv) >= 2:
-    nominatimstring = sys.argv[1]
+    city_name = sys.argv[1]
 if len(sys.argv) >= 3:
-    city_name = sys.argv[2]
+    export_data_slug = sys.argv[2]
 if len(sys.argv) >= 4:
     export_file_format = sys.argv[3]
+if len(sys.argv) >= 5:
+    city_boundary_file = sys.argv[4]
     
 print("Exporting " + export_file_format + " data for " + city_name)
 
-export_data_slug = slugify(city_name)
+export_data_slug = slugify(export_data_slug)
 
 for seed_point_type in ["grid", "rail"]:
     for ranking in ["betweenness_centrality", "closeness_centrality", "random"]:
@@ -49,7 +56,7 @@ for seed_point_type in ["grid", "rail"]:
             else: ens_string = ""
             print("\n" + "Exporting " + seed_point_type + ", " + ranking + ens_string)
             gbn.growbikenet(
-                city_name=nominatimstring,
+                city_name=city_name,
                 ranking=ranking,
                 seed_point_type=seed_point_type,
                 export_data=True,
@@ -58,4 +65,5 @@ for seed_point_type in ["grid", "rail"]:
                 export_file_format=export_file_format,
                 existing_network_spacing=ens,
                 export_data_slug=export_data_slug,
+                city_boundary_file=city_boundary_file,
             )
