599484cbbd13f2bbf216201555a06cf1528c302d
pess <147063686+marianamirandapessoa@users.noreply.github.com>
2026-08-24 11:31:05 +0200

    Update scripts batchexport_(...).py

--- scripts/growbikenet/batchexport_allcities.py
+++ scripts/growbikenet/batchexport_allcities.py
@@ -5,13 +5,104 @@ Script for exporting some growbikenet data for multiple cities.
 import pandas as pd
 import os
 from growbikenet.functions import slugify
+import subprocess
+import time
+import numpy as np
+import datetime
 
+# Import the list of cities
 df = pd.read_csv('../../cities/european_capitalsand100000pop.csv', 
                    sep = ';',)
 
+
+# Get date and hour to use for .txt files
+date = datetime.datetime.now()
+datestring = date.strftime("%Y%m%d_%H%M%S")
+
+
+# Create export_status.txt and error_log.txt files
+os.makedirs(f"./{datestring}", exist_ok=True)
+STATUS_FILE = f"{datestring}/export_status.txt"
+with open(STATUS_FILE, "w", encoding="utf-8") as f:
+    f.write("city_query\t\tcity_name\t\tordering\t\texisting_network_spacing\t\tseed_point_type\t\tstatus\n")
+
+ERROR_LOG = f"{datestring}/error_log.txt"
+with open(ERROR_LOG, "w", encoding="utf-8") as f:
+    f.write("city_query\t\tcity_name\t\tordering\t\texisting_network_spacing\t\tseed_point_type\t\ttraceback\n")
+
+
+# Define function to run batchexport_onecity.py
+def export_onecity(city_query, city_id, boundary_file):
+    """
+    Run batchexport_onecity.py, with suprocess in 'return' mode
+    """
+
+    args = [
+        "python",
+        "batchexport_onecity.py",
+        city_query,
+        city_id,
+        "geojson",
+        f"../../cities/boundaries/{city_id}.{boundary_file}",
+        f"../../cities/cityexport/growable_networks/{city_id}.gpkg",
+        f"../../cities/cityexport/bike_networks/{city_id}.gpkg",
+        f"../../cities/cityexport/rail_stations/{city_id}.gpkg",
+        f"../../cities/cityexport/schools/{city_id}.gpkg",
+        "True",
+        datestring
+    ]
+
+    logs = subprocess.run(
+        args,
+        text = True
+    )
+
+# Get start time for calculating running time
+start = time.time()
+
+
+# Run the loop for all cities
 for nominatimstring, city_name, country_code in zip(list(df.nominatim_query), list(df.name_en), list(df.country_code)):
     city_id = slugify(city_name)+"_"+slugify(country_code)
     if type(nominatimstring) is str:
-        os.system("python batchexport_onecity.py '"+nominatimstring+"' '"+city_id+"' geojson '../../cities/boundaries/"+city_id+".geojson' '../../cities/cityexport/street_networks/"+city_id+".gpkg' '../../cities/cityexport/bike_networks/"+city_id+".gpkg'")
-    else: # No entry is a nan in a df. Here we need to use a shape file. It must be in the folder cities/
-        os.system("python batchexport_onecity.py '"+city_name+"' '"+city_id+"' geojson '../../cities/boundaries/"+city_id+".geojson' '../../cities/cityexport/street_networks/"+city_id+".gpkg' '../../cities/cityexport/bike_networks/"+city_id+".gpkg'")
+        export_onecity(
+            nominatimstring,
+            city_id,
+            "geojson"
+        )
+        
+    else: # No entry is a nan in a df. Here we need to use a shape file. It must be in the folder cities/boundaries
+        if os.path.isfile("../../cities/boundaries/"+city_id+".geojson"):
+            export_onecity(
+                city_name, 
+                city_id,
+                "geojson"
+            )
+        else:
+            export_onecity(
+                city_name, 
+                city_id,
+                "shp"
+            )
+    print()
+
+
+# Calculate running time
+end = time.time()
+running_time = end - start
+
+days = int(running_time/(24*60*60))
+hours = int((running_time - days * (24*60*60)) / (60*60))
+minutes = int((running_time - days * (24*60*60) - hours * (60*60)) / (60))
+seconds = round(running_time - days * (24*60*60) - hours * (60*60) - minutes * 60, 2)
+
+with open(STATUS_FILE, "a", encoding="utf-8") as f:
+    f.write(f"TOTAL RUNNING TIME: {days} days, {hours} hours, {minutes} minutes, {seconds} seconds")
+
+
+# Create environment.yaml
+subprocess.run(
+        f'conda env export --no-builds | grep -v "^prefix: " > {datestring}/environment.yaml',
+        shell = True,
+        text = True
+    )
--- scripts/growbikenet/batchexport_onecity.py
+++ scripts/growbikenet/batchexport_onecity.py
@@ -13,43 +13,60 @@ city_boundary : (str | None), default None
     If not set to None, the study area will be selected from the (Multi)Polygon provided in the city_boundary shape or gpkg file, ideally in unprojected latitude-longitude degrees (EPSG:4326), but EPSG:3857 also works. For example, "./tests/test_data/copenhagen_city_boundary.shp".
 street_network : str
     The street network is loaded from this file. Must be a gpkg file in unprojected crs EPSG:4326 with layers nodes and edges, with the structure that an undirected osmnx street network g has after saved via ox.io.save_graph_geopackage().
-bike_network : st
+bike_network : str
     The existing bike network is loaded from this file. Must be a gpkg file in unprojected crs EPSG:4326 with layers nodes and edges, with the structure that an undirected osmnx bike network has after saved via ox.io.save_graph_geopackage().
+rail_stations : str 
+    The list of rail stations is loaded from this file. Must be a gpkg file in unprojected crs EPSG:4326.
+schools : str | None, default None
+    The list of schools is loaded from this file. Must be a gpkg file in unprojected crs EPSG:4326.
+check_files : bool, default False
+    If set to False, data is exported regardless if there is already a file with the same name in the "./results" directory
 
 Notes
 -------
-Exports data into four files:
-[slug]-betweenness_centrality-grid.gpkg
-[slug]-betweenness_centrality-rail.gpkg
-[slug]-closeness_centrality-grid.gpkg
-[slug]-closeness_centrality-rail.gpkg
+Exports data into 12 files:
+[slug]-growbikenet-[ordering]-[exist_nw]-[seed_point_type]-no_overlaps.geojson
     Data is saved into the current working directory.
-    slug is a string id created out of city_name.
+        [slug] is a string id created out of city_name.
+        [ordering] is 'betweenness' or 'closeness'.
+        [exist_nw] is 'from_scratch' or 'from_bikenw'.
+        [seed_point_type] is 'grid_(square|triangle)', 'rail' or 'school'.
 
 Examples
 --------
->>> python batchexport_onecity.py Barcelona Barcelona_es geojson ../../cities/boundaries/barcelona_es.geojson ../../cities/cityexport/street_networks/barcelona_es.gpkg ../../cities/cityexport/bike_networks/barcelona_es.gpkg
+>>> python batchexport_onecity.py Barcelona Barcelona_es geojson ../../cities/boundaries/barcelona_es.geojson ../../cities/cityexport/growable_networks/barcelona_es.gpkg ../../cities/cityexport/bike_networks/barcelona_es.gpkg ../../cities/cityexport/rail_stations/barcelona_es.gpkg ../../cities/cityexport/schools/barcelona_es.gpkg False
 """
 
 # WHICH DATA TO EXPORT?
 export_seed_point_types = ['auto', 'rail', 'school'] # Full array: ['auto', 'rail', 'school']
-export_rankings = ['betweenness_centrality', 'closeness_centrality'] # Full array: ['betweenness_centrality', 'closeness_centrality', 'random']
+export_orderings = ['betweenness', 'closeness'] # Full array: ['betweenness', 'closeness', 'random']
 export_existing_network_spacings = [None, 'auto'] # Full array: [None, 'auto']
 
 # Main
 import growbikenet as gbn
 import sys
 from growbikenet.functions import slugify
-from growbikenet import constants
 from growbikenet import settings
+import os
+import traceback
+import re
 
+# Variables
 city_query = "Badalona"
 city_name = "badalona_es"
 settings.export_file_format = "geojson"
 city_boundary = "../../cities/boundaries/badalona_es.geojson"
-street_network = "../../cities/cityexport/street_networks/badalona_es.gpkg"
+growable_network = "../../cities/cityexport/growable_networks/badalona_es.gpkg"
 bike_network = "../../cities/cityexport/bike_networks/badalona_es.gpkg"
+rail_stations = "../../cities/cityexport/rail_stations/badalona_es.gpkg"
+schools = "../../cities/cityexport/schools/badalona_es.gpkg"
+check_files = False
 
+# Variables for batch export
+datestring = ""
+export_status = False
+
+# Assign inputs to variables
 if len(sys.argv) >= 2:
     city_query = sys.argv[1]
 if len(sys.argv) >= 3:
@@ -59,30 +76,129 @@ if len(sys.argv) >= 4:
 if len(sys.argv) >= 5:
     city_boundary = sys.argv[4]
 if len(sys.argv) >= 6:
-    street_network = sys.argv[5]
+    growable_network = sys.argv[5]
 if len(sys.argv) >= 7:
     bike_network = sys.argv[6]
+if len(sys.argv) >= 8:
+    rail_stations = sys.argv[7]
+if len(sys.argv) >= 9:
+    schools = sys.argv[8]
+if len(sys.argv) >= 10:
+    check_files = sys.argv[9]
 
+# The 11th argument (date of export) is given when there is a batch export for more than 1 city
+# -> the files are always checked and the information is always logged into .txt files
+if len(sys.argv) >= 11: 
+    check_files = True 
+    datestring = sys.argv[10]
+    export_status = True
+    STATUS_FILE = f"{datestring}/export_status.txt"
+    ERROR_LOG = f"{datestring}/error_log.txt"
+
+
+# Retrieve the list of already generated files
+if check_files == True:
+    results_dir = "./results"
+    if os.path.exists(results_dir):
+        files = sorted(os.listdir(os.fsencode(results_dir)))
+        generated_files = [os.fsdecode(file) for file in files]
+    else:
+        generated_files = []
+
+
+# Initialise city_name and import_files
 city_name = slugify(city_name)
+import_files={"city_boundary": city_boundary, "growable_network": growable_network}
 
-for seed_point_type in export_seed_point_types:
-    for ranking in export_rankings:
-        for existing_network_spacing in export_existing_network_spacings:
+for seed_point_type in export_seed_point_types: # [auto, rail , school]
+
+    # Adjust the input and name of [seed_point_type]
+    if seed_point_type == 'auto':
+        seed_point_type_input = 'auto'
+        import_files.pop('seed_points', None)
+    elif seed_point_type == 'rail':
+        seed_point_type_input = 'file'
+        settings.seed_point_type_name = 'rail'
+        import_files['seed_points'] = rail_stations
+    elif seed_point_type == 'school':
+        seed_point_type_input = 'file'
+        settings.seed_point_type_name = 'school'
+        import_files['seed_points'] = schools
+
+
+    for ordering in export_orderings: # [betweenness, closeness]
+
+        for existing_network_spacing in export_existing_network_spacings: # [None, auto]
+
             if seed_point_type == 'auto' and existing_network_spacing == 'auto':
                 seed_point_linking = 'triangulate_delaunay'
             else:
                 seed_point_linking = 'auto'
-            gbn.growbikenet(
-                city_query,
-                ranking=ranking,
-                seed_point_type=seed_point_type,
-                seed_point_linking=seed_point_linking,
-                export_data=True,
-                export_plots=False,
-                existing_network_spacing=existing_network_spacing,
-                export_data_slug=city_name,
-                import_files={"city_boundary": city_boundary, "street_network": street_network, "bike_network": bike_network},
-            )
+
+
+            # Determine the name of [exnw_string] and if to include the bike_network
+            if existing_network_spacing:
+                exnw_string = "from_bikenw"
+                import_files['bike_network'] = bike_network
+            else:
+                exnw_string = "from_scratch"
+                import_files.pop('bike_network', None)
+
+
+            # Check if there is already a file for this setup (if check_file is True)
+            if check_files == True:
+                if seed_point_type == "auto":
+                    pattern = re.compile(
+                        rf"{re.escape(city_name)}-growbikenet-{re.escape(ordering)}-"
+                        rf"{re.escape(exnw_string)}-grid_(square|triangle)-no_overlaps\.{re.escape(settings.export_file_format)}"
+                    )
+                    found_file = any(pattern.fullmatch(f) for f in generated_files)
+                else:
+                    fname = f"{city_name}-growbikenet-{ordering}-{exnw_string}-{seed_point_type}-no_overlaps.{settings.export_file_format}"
+                    found_file = fname in generated_files
+            else:
+                found_file = False
+            
+
+
+            if found_file:
+                print(f"Found file for: {city_name}, {ordering}, {exnw_string}, {seed_point_type}")
+                if export_status:
+                    with open(STATUS_FILE, "a", encoding="utf-8") as f:
+                        f.write("\t\t".join(str(x) for x in [city_query, city_name, ordering, existing_network_spacing, seed_point_type, "✅"]) + "\n")
+
+            else:
+                print(f"No file for: {city_name}, {ordering}, {exnw_string}, {seed_point_type}")
+
+                try:
+                    gbn.growbikenet(
+                        city_query,
+                        ordering=ordering,
+                        seed_point_type=seed_point_type_input,
+                        seed_point_linking=seed_point_linking,
+                        export_data=True,
+                        export_plots=False,
+                        existing_network_spacing=existing_network_spacing,
+                        city_id=city_name,
+                        import_files=import_files,
+                    )
+                    if export_status:
+                        with open(STATUS_FILE, "a", encoding="utf-8") as f:
+                            f.write("\t\t".join(str(x) for x in [city_query, city_name, ordering, existing_network_spacing, seed_point_type, "✅"])+ "\n")
+
+                except Exception as e:
+                    status_error = f"{type(e).__name__}: {e}"
+                    traceback_error = traceback.format_exc()
+                    if export_status:
+                        with open(STATUS_FILE, "a", encoding="utf-8") as f:
+                            f.write("\t\t".join(str(x) for x in [city_query, city_name, ordering, existing_network_spacing, seed_point_type, status_error])+ "\n")
+                        with open(ERROR_LOG, "a", encoding="utf-8") as f:
+                            f.write("\t\t".join(str(x) for x in [city_query, city_name, ordering, existing_network_spacing, seed_point_type, traceback_error])+ "\n")
+                    else:
+                        print(f"{status_error}")
+                        print(traceback_error)
+    
+
 # Temporary hack to replace generated with real city boundary
 import shutil
 shutil.copyfile(city_boundary, "./results/"+city_name+"-city_boundary.geojson")
